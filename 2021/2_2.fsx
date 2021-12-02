40b353a9302644b23c044fbd499e219b3a8be1bc
Martin Gjerde <martin@gjerde.nu>
2021-12-02 11:14:34 +0100

    Even more functional - As few lets as possible too

--- 2021/2_2.fsx
+++ 2021/2_2.fsx
@@ -1,12 +1,10 @@
 open System.IO
 
-let lines = File.ReadAllLines(".\Input\2.txt") |> Array.map string
-
 let mutable horizontalPosition = 0
 let mutable depth = 0
 let mutable aim = 0
 
-let calculateNewPosition (direction, value) =
+let updatePosition (direction, value) =
     match direction with
     | "forward" ->  horizontalPosition <- horizontalPosition + value
                     depth <- depth + (aim * value)
@@ -14,11 +12,12 @@ let calculateNewPosition (direction, value) =
     | "up" -> aim <- aim - value
     | _ -> ()
 
-lines 
+File.ReadAllLines(".\Input\2.txt") 
+|> Array.map string
 |> Array.map (fun line ->  
                 let arr = line.Split ' '
                 arr.[0], int(arr.[1]))
-|> Array.iter calculateNewPosition
+|> Array.iter updatePosition
 
 printfn "The forward position is: %d" horizontalPosition
 printfn "The depth is: %d" depth
